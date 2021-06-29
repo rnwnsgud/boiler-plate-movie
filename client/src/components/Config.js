00b3d12db7e1d3002997688feff269d58d68cff8
rnwnsgud <rnwnsgud90@naver.com>
2021-06-30 00:15:39 +0900

    init git

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https:api.themoviedb.org/3/";
+export const API_KEY = "ead4499472d82073921565d124cb3394";
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
